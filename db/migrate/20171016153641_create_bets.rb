831cf60fad4dc7f296132707d322ba492b04862f
Milo Todt <milotodt@gmail.com>
2017-10-16 08:40:50 -0700

    DB started

--- db/migrate/20171016153641_create_bets.rb
+++ db/migrate/20171016153641_create_bets.rb
@@ -1,6 +1,10 @@
 class CreateBets < ActiveRecord::Migration[5.1]
   def change
     create_table :bets do |t|
+      t.string :date
+      t.string :name
+      t.string :odds
+      t.string :margin
 
       t.timestamps
     end
